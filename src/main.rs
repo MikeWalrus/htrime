d945e4bf4caeba7b14237081555942846242d00f
Liao Junxuan <mikeljx@126.com>
2023-11-24 16:26:31 +0800

    wayland client initialization

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,15 @@ use std::ptr::{null, null_mut};
 use libc::{ftruncate, mmap, shm_open, shm_unlink, O_CREAT, O_EXCL, O_RDWR};
 use wayland_client::protocol::wl_buffer::WlBuffer;
 use wayland_client::protocol::wl_compositor::WlCompositor;
+use wayland_client::protocol::wl_pointer::WlPointer;
 use wayland_client::protocol::wl_seat::WlSeat;
 use wayland_client::protocol::wl_shm::WlShm;
 use wayland_client::protocol::wl_shm_pool::WlShmPool;
-use wayland_client::protocol::wl_surface;
+use wayland_client::protocol::wl_surface::WlSurface;
+use wayland_client::protocol::{wl_display, wl_surface};
 use wayland_client::{delegate_noop, EventQueue, Proxy};
 use wayland_client::{protocol::wl_registry, Connection, Dispatch, QueueHandle};
 
-use wayland_egl::WlEglSurface;
-use wayland_protocols::xdg;
-use wayland_protocols::xdg::shell::client::xdg_toplevel::XdgToplevel;
-use wayland_protocols::xdg::shell::client::xdg_wm_base::XdgWmBase;
 use wayland_protocols_misc::zwp_input_method_v2::client::zwp_input_method_manager_v2::ZwpInputMethodManagerV2;
 use wayland_protocols_misc::zwp_input_method_v2::client::zwp_input_method_v2::ZwpInputMethodV2;
 use wayland_protocols_misc::zwp_input_method_v2::client::zwp_input_popup_surface_v2::ZwpInputPopupSurfaceV2;
@@ -25,15 +23,46 @@ use wayland_protocols_misc::zwp_input_method_v2::client::{
 
 const NAME: &str = "htrime";
 
+struct Globals {
+    input_method_manager: Option<ZwpInputMethodManagerV2>,
+    seat: Option<WlSeat>,
+    compositor: Option<WlCompositor>,
+    shm: Option<WlShm>,
+}
+
+struct State {
+    shm: WlShm,
+    shm_pool: WlShmPool,
+    pointer: WlPointer,
+    input_method: ZwpInputMethodV2,
+    popup: ZwpInputPopupSurfaceV2,
+    surface: WlSurface,
+    cairo_surface: cairo::ImageSurface,
+}
+
+impl Globals {
+    fn new() -> Self {
+        Self {
+            input_method_manager: None,
+            seat: None,
+            compositor: None,
+            shm: None,
+        }
+    }
+}
+
 fn main() {
-    init()
+    let (mut state, mut wayland_queue) = init();
+    loop {
+        wayland_queue.blocking_dispatch(&mut state).unwrap();
+    }
 }
 
 fn shm_file(size: i32) -> BorrowedFd<'static> {
     unsafe {
         let name = CString::new(NAME).unwrap();
         let name_ptr = name.as_ptr();
-        let fd = shm_open(name_ptr, O_RDWR | O_CREAT | O_EXCL, 0600);
+        let fd = shm_open(name_ptr, O_RDWR | O_CREAT | O_EXCL, 0o600);
         if fd < 0 {
             panic!("shm_open failed")
         }
@@ -55,43 +84,43 @@ fn shm_file(size: i32) -> BorrowedFd<'static> {
     }
 }
 
-fn init() {
+fn init() -> (State, EventQueue<State>) {
     let conn = Connection::connect_to_env().unwrap();
-    let mut event_queue: EventQueue<State> = conn.new_event_queue();
-    let handle = event_queue.handle();
-    println!("get registry");
+    let mut registry_queue: EventQueue<Globals> = conn.new_event_queue();
+    let registry_qh = registry_queue.handle();
 
-    conn.display().get_registry(&handle, ());
+    let wayland_queue: EventQueue<State> = conn.new_event_queue();
+    let wayland_qh = wayland_queue.handle();
 
-    let mut state = State {
+    conn.display()
+        .get_registry(&registry_qh, wayland_qh.clone());
+
+    let mut state = Globals {
         input_method_manager: None,
         seat: None,
         compositor: None,
         shm: None,
-        xdg_wm_base: None,
     };
-    event_queue.roundtrip(&mut state).unwrap();
+    registry_queue.roundtrip(&mut state).unwrap();
 
-    let compositor = state.compositor.as_ref().unwrap();
-    let manager = state.input_method_manager.as_ref().unwrap();
-    let seat = state.seat.as_ref().unwrap();
+    let compositor = state.compositor.unwrap();
+    let manager = state.input_method_manager.unwrap();
+    let seat = state.seat.unwrap();
 
-    let xdg_wm_base = state.xdg_wm_base.as_ref().unwrap();
-
-    let shm = state.shm.as_ref().unwrap();
+    let shm = state.shm.unwrap();
     let size = 1024 * 1024 * 1024;
     let fd = shm_file(size);
-    let pool = shm.create_pool(fd, size, &handle, ());
+    let shm_pool = shm.create_pool(fd, size, &wayland_qh, ());
     let width = 500;
     let height = 100;
     let stride = width * 4;
-    let buffer = pool.create_buffer(
+    let buffer = shm_pool.create_buffer(
         0,
         width,
         height,
         stride,
         wayland_client::protocol::wl_shm::Format::Argb8888,
-        &handle,
+        &wayland_qh,
         (),
     );
     let buffer_size = stride * height * width;
@@ -109,30 +138,17 @@ fn init() {
     let data: &mut [u8] =
         unsafe { std::slice::from_raw_parts_mut(data as *mut u8, buffer_size as usize) };
 
-    let surface = compositor.create_surface(&handle, ());
-    // let xdg_surface = xdg_wm_base.get_xdg_surface(&surface, &handle, ());
-    // let toplevel = xdg_surface.get_toplevel(&handle, ());
-    // toplevel.set_title("title".to_string());
-    // surface.commit();
+    let pointer = seat.get_pointer(&wayland_qh, ());
 
-    // event_queue.roundtrip(&mut state).unwrap();
+    let surface = compositor.create_surface(&wayland_qh, ());
 
     surface.attach(Some(&buffer), 0, 0);
     surface.damage(0, 0, i32::MAX, i32::MAX);
     surface.commit();
 
+    let input_method = manager.get_input_method(&seat, &wayland_qh, ());
 
-    let mut queue: EventQueue<InputMethodAvailable> = conn.new_event_queue();
-    let handle1 = queue.handle();
-
-    let input_method = manager.get_input_method(&seat, &handle1, ());
-    let mut input_method_available = InputMethodAvailable(true);
-    queue.roundtrip(&mut input_method_available).unwrap();
-    if !input_method_available.0 {
-        panic!("Input method unavailable.");
-    }
-
-    let popup = input_method.get_input_popup_surface(&surface, &handle, ());
+    let popup = input_method.get_input_popup_surface(&surface, &wayland_qh, ());
 
     let cairo_surface =
         cairo::ImageSurface::create_for_data(data, cairo::Format::ARgb32, width, height, stride)
@@ -146,30 +162,31 @@ fn init() {
     surface.damage(0, 0, i32::MAX, i32::MAX);
     surface.commit();
 
-    loop {
-        event_queue.blocking_dispatch(&mut state).unwrap();
-    }
-}
-
-struct State {
-    input_method_manager: Option<ZwpInputMethodManagerV2>,
-    seat: Option<WlSeat>,
-    compositor: Option<WlCompositor>,
-    shm: Option<WlShm>,
-    xdg_wm_base: Option<xdg::shell::client::xdg_wm_base::XdgWmBase>,
+    (
+        State {
+            shm,
+            pointer,
+            input_method,
+            surface,
+            cairo_surface,
+            popup,
+            shm_pool,
+        },
+        wayland_queue,
+    )
 }
 
 const ZWP_INPUT_METHOD_MANAGER_V2_VERSION: u32 = 1;
 const WL_SEAT_VERSION: u32 = 8;
 
-impl Dispatch<wl_registry::WlRegistry, ()> for State {
+impl Dispatch<wl_registry::WlRegistry, QueueHandle<State>> for Globals {
     fn event(
         state: &mut Self,
         registry: &wl_registry::WlRegistry,
         event: wl_registry::Event,
-        _: &(),
+        handle: &QueueHandle<State>,
         _: &Connection,
-        handle: &QueueHandle<Self>,
+        _: &QueueHandle<Self>,
     ) {
         if let wl_registry::Event::Global {
             name, interface, ..
@@ -182,28 +199,16 @@ impl Dispatch<wl_registry::WlRegistry, ()> for State {
                     state.input_method_manager = Some(manager);
                 }
                 "wl_seat" => {
-                    let seat: WlSeat =
-                        registry.bind::<WlSeat, (), State>(name, WL_SEAT_VERSION, handle, ());
+                    let seat: WlSeat = registry.bind(name, WL_SEAT_VERSION, handle, ());
                     state.seat = Some(seat);
                 }
                 "wl_compositor" => {
-                    let compositor: WlCompositor =
-                        registry.bind::<WlCompositor, (), State>(name, 4, handle, ());
+                    let compositor: WlCompositor = registry.bind(name, 4, handle, ());
                     state.compositor = Some(compositor);
                 }
                 "wl_shm" => {
-                    let shm: WlShm = registry.bind::<WlShm, (), State>(name, 1, handle, ());
+                    let shm: WlShm = registry.bind(name, 1, handle, ());
                     state.shm = Some(shm);
-                }
-                "xdg_wm_base" => {
-                    let xdg_wm_base: xdg::shell::client::xdg_wm_base::XdgWmBase =
-                        registry.bind::<xdg::shell::client::xdg_wm_base::XdgWmBase, (), State>(
-                            name,
-                            1,
-                            handle,
-                            (),
-                        );
-                    state.xdg_wm_base = Some(xdg_wm_base);
                 }
                 _ => {}
             }
@@ -211,47 +216,82 @@ impl Dispatch<wl_registry::WlRegistry, ()> for State {
     }
 }
 
-delegate_noop!(State: ignore WlSeat);
-delegate_noop!(State: ignore ZwpInputMethodManagerV2);
-delegate_noop!(State: ignore WlCompositor);
-delegate_noop!(State: ignore WlShm);
-delegate_noop!(State: ignore WlShmPool);
-delegate_noop!(State: ignore WlBuffer);
-delegate_noop!(State: ignore XdgToplevel);
-
-impl Dispatch<XdgWmBase, ()> for State {
+impl Dispatch<WlCompositor, ()> for State {
     fn event(
         state: &mut Self,
-        _: &XdgWmBase,
-        event: xdg::shell::client::xdg_wm_base::Event,
-        _: &(),
-        _: &Connection,
-        _: &QueueHandle<Self>,
+        proxy: &WlCompositor,
+        event: <WlCompositor as Proxy>::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
     ) {
-        println!("xdg_wm_base event");
-        if let xdg::shell::client::xdg_wm_base::Event::Ping { serial } = event {
-            state.xdg_wm_base.as_ref().unwrap().pong(serial);
-        }
+        println!("compositor event");
+    }
+}
+impl Dispatch<WlShmPool, ()> for State {
+    fn event(
+        state: &mut Self,
+        proxy: &WlShmPool,
+        event: <WlShmPool as Proxy>::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        println!("shm pool event");
+    }
+}
+impl Dispatch<WlBuffer, ()> for State {
+    fn event(
+        state: &mut Self,
+        proxy: &WlBuffer,
+        event: <WlBuffer as Proxy>::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        println!("buffer event")
+    }
+}
+impl Dispatch<ZwpInputMethodManagerV2, ()> for State {
+    fn event(
+        state: &mut Self,
+        proxy: &ZwpInputMethodManagerV2,
+        event: <ZwpInputMethodManagerV2 as Proxy>::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        println!("input method manager event");
     }
 }
 
-impl Dispatch<xdg::shell::client::xdg_surface::XdgSurface, ()> for State {
+impl Dispatch<WlSeat, ()> for State {
     fn event(
         state: &mut Self,
-        xdg_surface: &xdg::shell::client::xdg_surface::XdgSurface,
-        event: xdg::shell::client::xdg_surface::Event,
-        _: &(),
-        _: &Connection,
-        _: &QueueHandle<Self>,
+        proxy: &WlSeat,
+        event: <WlSeat as Proxy>::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
     ) {
-        println!("xdg_surface event");
-        if let xdg::shell::client::xdg_surface::Event::Configure { serial } = event {
-            xdg_surface.ack_configure(serial);
-        }
+        println!("seat event");
     }
 }
 
-impl Dispatch<wl_surface::WlSurface, ()> for State {
+impl Dispatch<WlShm, ()> for State {
+    fn event(
+        state: &mut Self,
+        proxy: &WlShm,
+        event: <WlShm as Proxy>::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        println!("shm event");
+    }
+}
+
+impl Dispatch<wl_surface::WlSurface, ()> for Globals {
     fn event(
         state: &mut Self,
         _: &wl_surface::WlSurface,
@@ -264,9 +304,7 @@ impl Dispatch<wl_surface::WlSurface, ()> for State {
     }
 }
 
-struct InputMethodAvailable(bool);
-
-impl Dispatch<ZwpInputMethodV2, ()> for InputMethodAvailable {
+impl Dispatch<ZwpInputMethodV2, ()> for State {
     fn event(
         state: &mut Self,
         _: &ZwpInputMethodV2,
@@ -275,8 +313,9 @@ impl Dispatch<ZwpInputMethodV2, ()> for InputMethodAvailable {
         _: &Connection,
         _: &QueueHandle<Self>,
     ) {
+        println!("input method event");
         if let zwp_input_method_v2::Event::Unavailable = event {
-            state.0 = false;
+            panic!("Input method unavailable.")
         }
     }
 }
@@ -302,5 +341,31 @@ impl Dispatch<ZwpInputPopupSurfaceV2, ()> for State {
         {
             println!("x: {}, y: {}, width: {}, height: {}", x, y, width, height)
         }
+    }
+}
+
+impl Dispatch<WlSurface, ()> for State {
+    fn event(
+        state: &mut Self,
+        proxy: &WlSurface,
+        event: <WlSurface as Proxy>::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        println!("surface event");
+    }
+}
+
+impl Dispatch<WlPointer, ()> for State {
+    fn event(
+        state: &mut Self,
+        proxy: &WlPointer,
+        event: <WlPointer as Proxy>::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        println!("pointer event");
     }
 }
